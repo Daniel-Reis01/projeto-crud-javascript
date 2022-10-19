7ff0b2b65b6fddfdce89d3796fc3e20333b38cef
daniel almeida reis <daniel.7t@hotmail.com>
2022-10-19 01:36:47 -0300

    atualizacao dos estudos

--- pratica1/pratica.js
+++ pratica1/pratica.js
@@ -1,3 +1,19 @@
-document.querySelector('html').onclick = function() {
-    alert('bem vindo a o  ambiente de estudos javscript');
+// document.querySelector('html').onclick = function() {
+//     alert('bem vindo a o  ambiente de estudos javscript');
+// }
+
+
+let minhaImagem = document.querySelector("img");
+
+minhaImagem.onclick = function() {
+
+    let meuSrc = minhaImagem.getAttribute('src');
+
+    if (meuSrc === 'file:\Users\Pichau\Desktop\projeto crud javascript\img\maleta.png') {
+        minhaImagem.setAttribute('src','file:\Users\Pichau\Desktop\projeto crud javascript\img\pc.png');
+    }else{
+        minhaImagem.setAttribute('src','file:\Users\Pichau\Desktop\projeto crud javascript\img\maleta.png');
+
+    }
 }
+
